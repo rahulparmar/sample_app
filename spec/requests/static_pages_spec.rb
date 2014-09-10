e93b48c18439f80b5e44ce3d9bb5c9b5fbf107a7
Rahul Parmar <rahul.parmar@gmail.com>
2014-09-10 15:56:38 -0700

    Finish routes and layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,58 +1,42 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+	subject {page}
+
 	describe "Home page" do
 
-		it "should have content 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
+		before {visit root_path}
 
-		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial App")
-		end
-
-		it "should not have custom title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('| Home')
-		end
+		it {should have_content ('Sample App')}
+		it {should have_title(full_title(''))}
+		it {should_not have_title ('|Home')}
 	end
 
 	describe "Help page" do
-		
-		it "should have content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
 
+		before {visit help_path}
 
-		it "should have the right title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("Help")
-		end
+		it {should have_content ('Help')}
+		it {should have_title(full_title(''))}
+		it {should_not have_title ('|Help')}
 	end
-
+		
 	describe "About page" do
-		it "should have content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
 
+		before {visit about_path}
 
-		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("About Us")
-		end
+		it {should have_content ('About')}
+		it {should have_title(full_title(''))}
+		it {should_not have_title ('|About')}
 	end
 
 	describe "Contact page" do
-		it "should have content 'Contact Us'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact Us')
-		end
+		before {visit contact_path}
+
+		it {should have_content ('Contact')}
+		it {should have_title(full_title(''))}
+		it {should_not have_title ('|Contact')}
 	end
-
-
 end
 
